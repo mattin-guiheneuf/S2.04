7cad28f72cd1b0db7db587cbb48f7b97696ab552
Mattin Guiheneuf <mattin.guiheneuf@orange.fr>
2023-05-20 10:57:33 +0200

    Tentative Réglage Pb

---
+++ S2.04/CodePython/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 20 10:22:02 2023
+
+@author: matti
+"""
+
---
+++ S2.04/CodePython/main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import P2_MenuPrincipal as menuP
+menuP.menuPrincipal()
